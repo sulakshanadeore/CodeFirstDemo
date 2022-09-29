2e2d3922a3affa8d4705ebd040ebc8a07c2384d6
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-29 10:50:35 +0530

    Add project files.

---
+++ CodeFirstDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstDALDemo;
+namespace CodeFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContext context = new MyContext();
+            context.StudentsTable.Add(new Student{Rollno=1,Studname="Amit" });
+            context.SaveChanges();
+            Console.WriteLine("done");
+            Console.Read();
+        }
+    }
+}
