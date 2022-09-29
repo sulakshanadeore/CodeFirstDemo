ce2351475b8b1d2ec8a6d33f0ae12b8c686115ed
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-29 10:52:15 +0530

    code first

--- CodeFirstDemo/Program.cs
+++ CodeFirstDemo/Program.cs
@@ -11,7 +11,7 @@ namespace CodeFirstDemo
         static void Main(string[] args)
         {
             MyContext context = new MyContext();
-            context.StudentsTable.Add(new Student{Rollno=1,Studname="Amit" });
+            context.StudentsTable.Add(new Student{Rollno=1,Studname="Amit",City="Pune",DOB=new DateTime(2022,11,11) });
             context.SaveChanges();
             Console.WriteLine("done");
             Console.Read();
